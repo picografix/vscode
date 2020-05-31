c7ada5be168c4d37435cd9f5fdd5f0967c0a4e60
gauransh-soni <gauranshiitd@gmail.com>
2020-05-31 19:41:12 +0530

    various html templates addeed

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+posts = [
+    {
+        'author':'Gauransh Soni',
+        'title':'Blog Post',
+        'Content': 'First post Content'
+        'date_posted': 'May 31,2020'
+    },
+    {
+        'author':'Picografix',
+        'title':'Blog  2',
+        'Content': 'Second post Content'
+        'date_posted': 'June 1,2020'
+    }
+]
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/index.html',context)
 def about(request):
-    return HttpResponse('<h2>Hello THis is Gopu Soni</h2>')
+    return render(request,'blog/about.html')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,5 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-import django_heroku
-django_heroku.settings(locals())
